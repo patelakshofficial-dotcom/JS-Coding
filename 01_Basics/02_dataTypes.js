5e9aa3c1026097a86600a4b0f2eef5abb8d8ff3a
AKSH PATEL <patelakshofficial@gmail.com>
2025-09-02 10:49:56 +0000

    new files

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict";  // treat all js code as newer version 
+
+// alert("hello") // we are using nodejs, not browser. 
+
+//code readability 
+
+
+console.log(3+3);  
+console.log('aksh');
+
+let name=" aksh"
+
+let age = 20
+
+let login= true
+
+
+// number  ==> 2 to power 53 
+// bigint  
+//string ==>"string"
+//boolean ==> true/false
+//null  ==> standalone value 
+// undefined ==> 
+//symbol ==> unique
+
+
+//non primitive datatype
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+//assignment 
---
+++ 01_Basics/03_conversionOprations.js
@@ -0,0 +1,19 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInnumber = Number(score)
+console.log(typeof valueInnumber);
+
+console.log(valueInnumber);
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1 
+
+
+let isLoggedIn = ''
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
