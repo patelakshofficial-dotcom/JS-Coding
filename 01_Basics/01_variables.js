9461c26dda7bfa0081c79c442620709ad4ba81e3
AKSH PATEL <patelakshofficial@gmail.com>
2025-09-02 10:18:11 +0000

    NA

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144534
+let accountEmail = "akshpatel@gmail.com"
+var accountPassword = "12345678"
+accountCity = "vadodara"
+let accountState;
+
+// accountId = 2 // not vaild 
+
+console.log(accountId);
+accountEmail = "aksh@gmail.com"
+accountPassword = "212111"
+accountCity = "surat"
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
+//scope is this brackets called as scope {([])} in javascript
+
+/* 
+prefer not to use var because of issue in block scope and functinal scope
+*/ 
+
